dd9e64fa5fec5003f84c49759e66b35f8a678f3b
joki <mathisjoelk@outlook.de>
2026-02-22 16:16:59 +0100

    add fterm plugin

---
+++ lua/plugins/fterm.lua
@@ -0,0 +1,8 @@
+return{
+    "numToStr/FTerm.nvim",
+
+    config = function()
+        vim.keymap.set('n', '<leader>t', '<CMD>lua require("FTerm").toggle()<CR>', { desc = 'Open Terminal' })
+       vim.keymap.set('t', '<leader>t', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', { desc = 'Open Terminal' })
+    end
+}
